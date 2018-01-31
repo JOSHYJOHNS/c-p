6a9da3911377453c3efb1b34de6694fc1d1dc28e
JOSHYJOHNS <35687903+JOSHYJOHNS@users.noreply.github.com>
2018-01-31 14:25:49 +0530

    Update pos.c

--- pos.c
+++ pos.c
@@ -2,14 +2,14 @@
  
 void main()
 {
-    int num;
+    int b;
  
     printf("Enter a number: \n");
-    scanf("%d", &num);
+    scanf("%d", &b);
     if (num > 0)
-        printf("%d is a positive number \n", num);
-    else if (num < 0)
-        printf("%d is a negative number \n", num);
+        printf("%d is a positive number \n", b);
+    else if (b < 0)
+        printf("%d is a negative number \n", b);
     else
         printf("0 is neither positive nor negative");
 }
